af3a6e7caf3a65da6021c0707b35ae6c2183af73
Khamid <khamid@MacBook-Air-Khamid.local>
2026-02-08 02:10:29 +0300

    fix: maximize mobile product image (75vh, cover) remove gradient

--- src/app/(shop)/product/[slug]/ProductPageClient.tsx
+++ src/app/(shop)/product/[slug]/ProductPageClient.tsx
@@ -89,7 +89,7 @@ export default function ProductPageClient({ product, similarProducts }: ProductP
                     <div className="lg:col-span-7 relative">
                         {/* Mobile: Full Screen Scroll Snap Swiper */}
                         {/* Desktop: Sticky Gallery */}
-                        <div className="sticky top-24 lg:top-32 h-[55vh] lg:h-[calc(100vh-160px)] w-full bg-white lg:rounded-[2.5rem] overflow-hidden">
+                        <div className="sticky top-0 lg:top-32 h-[75vh] lg:h-[calc(100vh-160px)] w-full bg-white lg:rounded-[2.5rem] overflow-hidden">
 
                             <div
                                 ref={scrollContainerRef}
@@ -102,12 +102,11 @@ export default function ProductPageClient({ product, similarProducts }: ProductP
                                             src={img}
                                             alt={`${product.name} - View ${idx + 1}`}
                                             fill
-                                            className="object-contain md:object-cover"
+                                            className="object-cover"
                                             priority={idx === 0}
                                             onClick={() => setLightboxOpen(true)}
                                         />
-                                        {/* Mobile Bottom Gradient for Text Readability */}
-                                        <div className="absolute inset-x-0 bottom-0 h-32 bg-gradient-to-t from-black/40 to-transparent lg:hidden pointer-events-none" />
+                                        {/* Gradient removed as requested for 'integral picture' look */}
                                     </div>
                                 ))}
                             </div>
